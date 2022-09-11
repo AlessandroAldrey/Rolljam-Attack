951b258cef0c03951cbfc6d945736f96da95c93b
AlessandroAldrey <90276552+AlessandroAldrey@users.noreply.github.com>
2022-09-11 19:14:26 +0200

    Delete wav-transform.py

--- wav-transform.py
+++
@@ -1,8 +0,0 @@
-import scipy.io.wavfile as wav
-
-rate,sample = wav.read("fragment_garage.wav")
-
-sample[sample>=0] = max(sample)
-sample[sample<0] = min(sample)
-
-wav.write("converted_garage.wav", rate, sample)
